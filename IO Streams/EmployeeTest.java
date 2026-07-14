63d0edd723d11b42af294816a312401a26c5f9b3
gorlakavya3 <gorlakavya3@gmail.com>
2026-07-14 14:46:35 +0530

    Refactor EmployeeTest for serialization and deserialization

--- IO Streams/EmployeeTest.java
+++ IO Streams/EmployeeTest.java
@@ -6,30 +6,23 @@ public class EmployeeTest {
     public static void main(String[] args) {
 
         try {
-            // Create Employee object
             Employee emp = new Employee(
                     "Kavya",
                     new Date(),
                     "IT",
                     "Software Engineer",
                     50000.0);
-
-            // Serialization
-            ObjectOutputStream oos =
-                    new ObjectOutputStream(new FileOutputStream("data"));
+            ObjectOutputStream oos =  new ObjectOutputStream(new FileOutputStream("data"));
             oos.writeObject(emp);
             oos.close();
 
             System.out.println("Employee object serialized successfully.");
 
-            // Deserialization
             ObjectInputStream ois =
                     new ObjectInputStream(new FileInputStream("data"));
 
             Employee e = (Employee) ois.readObject();
             ois.close();
-
-            // Display details
             System.out.println("\nEmployee Details:");
             System.out.println("Name        : " + e.getName());
             System.out.println("Date of Birth : " + e.getDateOfBirth());
